7e580309f3f723b670f826c46bf199be3d6390e7
sowamy <adlefevers2@yahoo.com>
2023-10-15 10:36:10 -0500

    update

--- code/index.js
+++ code/index.js
@@ -1,6 +1,7 @@
 var $menu = $('#menu-h')
 var $menuItems = $('#menu-items-h')
-var $logo = $('#logo-img-h')
+var $logo = $('#logo-h')
+let groupChildren = document.querySelector(".group2-pane").children
 
 $menu.click(function(){
   $menu.toggleClass('menu-small menu-big')
@@ -9,8 +10,27 @@ $menu.click(function(){
     $menuItems.css("display","block")
   } else {
     $menu.append($logo)
-    $logo.css("transform","translate(100px,-90px)")
     $menuItems.css("display","none")
   }
 })
 
+let options = {
+  root: null, // sense on viewport
+  rootMargin: "0px",
+  threshold: 0,
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("center")
+      entry.target.classList.add("fade-in")
+    } else {
+      entry.target.classList.remove("center")
+    }
+  });
+}, options);
+
+for(let child of groupChildren) {
+  observer.observe(child)
+}
